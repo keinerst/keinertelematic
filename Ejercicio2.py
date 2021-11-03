aee84061312b80ef2943b49971d76faaa9186be1
keinerst <93544272+keinerst@users.noreply.github.com>
2021-11-03 14:17:36 -0400

    Add files via upload

---
+++ Ejercicio2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from math import pi 
+r = float (input("Expresar radio de la circunferencia: "))
+l = 2 * pi * r
+a = pi * (r**2)
+print ("La longitud de la circunferenica es: ",l)
+print ("El area del circulo es: ",a)
